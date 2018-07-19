01db4b45a6926c6074770ad6ae7a8d5f6fadb3d0
Richard Weiss <richardweiss@richardweiss.org>
2018-07-20 00:02:49 +1000

    Slightly deeper nets

--- relgan/networks.py
+++ relgan/networks.py
@@ -6,9 +6,11 @@ from relgan.utils import Flatten, Reshape
 def Classifier():
     return torch.nn.Sequential(
         Flatten(),
-        torch.nn.Linear(28**2, 512),
+        torch.nn.Linear(28**2, 256),
         torch.nn.PReLU(),
-        torch.nn.Linear(512, 256),
+        torch.nn.Linear(256, 256),
+        torch.nn.PReLU(),
+        torch.nn.Linear(256, 256),
         torch.nn.PReLU(),
         torch.nn.Linear(256, 1),
     )
@@ -18,13 +20,16 @@ class Generator(torch.nn.Module):
     def __init__(self):
         super().__init__()
         self.generator = torch.nn.Sequential(
-            torch.nn.Linear(64, 512),
+            torch.nn.Linear(64, 128),
             torch.nn.PReLU(),
-            torch.nn.BatchNorm1d(512),
-            torch.nn.Linear(512, 512),
+            torch.nn.BatchNorm1d(128),
+            torch.nn.Linear(128, 128),
             torch.nn.PReLU(),
-            torch.nn.BatchNorm1d(512),
-            torch.nn.Linear(512, 1024),
+            torch.nn.BatchNorm1d(128),
+            torch.nn.Linear(128, 128),
+            torch.nn.PReLU(),
+            torch.nn.BatchNorm1d(128),
+            torch.nn.Linear(128, 1024),
             torch.nn.PReLU(),
             torch.nn.BatchNorm1d(1024),
             torch.nn.Linear(1024, 28**2),
